599f73769c2895a49dc17a014b8d1ce6669a6cec
Prometheuz <gabrielsampaiodejesus01042007@gmail.com>
2026-06-26 16:47:36 -0300

    feat: criando novo projeto para utilização do jpa

---
+++ spring-boot-essetials-jpa/src/main/java/br/com/atlas/spring_boot_essetials/SpringBootEssetialsApplication.java
@@ -0,0 +1,13 @@
+package br.com.atlas.spring_boot_essetials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEssetialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEssetialsApplication.class, args);
+	}
+
+}
---
+++ spring-boot-essetials-jpa/src/test/java/br/com/atlas/spring_boot_essetials/SpringBootEssetialsApplicationTests.java
@@ -0,0 +1,13 @@
+//package br.com.atlas.spring_boot_essetials;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SpringBootEssetialsApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
